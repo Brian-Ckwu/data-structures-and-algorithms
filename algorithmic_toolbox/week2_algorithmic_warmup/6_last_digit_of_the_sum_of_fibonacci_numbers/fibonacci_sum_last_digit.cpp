a2ec5fd13b6782961f0fa5eef27cda06d94af049
Brian Wu <b04401053@ntu.edu.tw>
2021-06-21 21:00:35 +0800

    Modify fibonacci_sum_last_digit.cpp: implement a more efficiency algorithm

--- algorithmic_toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ algorithmic_toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <functional>
+#include <cassert>
+
+using namespace std;
 
 int fibonacci_sum_naive(long long n) {
     if (n <= 1)
@@ -18,8 +22,46 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_sum_fast(long long n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    int pre = 0;
+    int cur = 1;
+    int sum = 1;
+
+    int temp = 0;
+    for (long long i = 2; i <= n; ++i) {
+        temp = cur;
+        cur = (pre + cur) % 10;
+        pre = temp;
+        sum = (sum + cur) % 10;
+    }
+
+    return sum;
+}
+
+void StressTest(long long n_max, function<int (long long)> func_naive,function<int (long long)> func_fast) {
+    int res_naive = 0, res_fast = 0;
+    
+    for (long long n = 0; n <= n_max; ++n) {
+        cout << "Test #" << n << "\n";
+
+        res_naive = func_naive(n);
+        res_fast = func_fast(n);
+
+        if (res_naive == res_fast) {
+            cout << "Same results: " << res_fast << "\n";
+        } else {
+            cout << "res_naive: " << res_naive << " res_fast: " << res_fast << "\n";
+            break;
+        }
+    }
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
