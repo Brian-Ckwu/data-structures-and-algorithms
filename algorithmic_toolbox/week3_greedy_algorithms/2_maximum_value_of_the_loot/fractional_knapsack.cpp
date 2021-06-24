f636b2bd0f1c281b05781e9e17febbe71d146280
Brian Wu <sonny40920710@gmail.com>
2021-06-24 17:44:55 +0800

    Implement fractional_knapsack

--- algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
+
+struct Item {
+  double weight;
+  double value;
+};
+
+bool by_wv_ratio(Item x, Item y) {
+  return (x.value / x.weight) > (y.value / y.weight);
+}
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+  // Value to return
   double value = 0.0;
+  // Vector of items
+  vector<Item> items(weights.size());
+  for (int i = 0; i < weights.size(); ++i) {
+    items[i].weight = weights[i];
+    items[i].value = values[i];
+  }
 
-  // write your code here
+  // Sort the items according to value/weight ratio
+  sort(items.begin(), items.end(), by_wv_ratio);
+
+  // Fill the knapsack from the first item to the last item
+  for (Item item : items) {
+    if (capacity > item.weight) {
+      value += item.value;
+      capacity -= item.weight;
+    } else {
+      value += capacity * (item.value / item.weight);
+      capacity -= capacity; // This line is not necessary in practice, though
+      break;
+    }
+  }
 
   return value;
 }
@@ -14,16 +44,16 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
 int main() {
   int n;
   int capacity;
-  std::cin >> n >> capacity;
+  cin >> n >> capacity;
   vector<int> values(n);
   vector<int> weights(n);
   for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
+    cin >> values[i] >> weights[i];
   }
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  cout.precision(10);
+  cout << optimal_value << endl;
   return 0;
 }
