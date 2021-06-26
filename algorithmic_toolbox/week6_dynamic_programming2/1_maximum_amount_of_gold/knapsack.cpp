e35afdb98c91bbc02c67760f7d044a3e0eb2df8b
Brian Wu <sonny40920710@gmail.com>
2021-06-26 23:13:12 +0800

    Complete programming assignment: maximum amount of gold (0/1 knapsack problem)

--- algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+// Could the algorithm be different 
+int optimal_weight(int W, const vector<int>& w) {
+  // Vector for storing maximum values of subproblems
+  // Rows: number of items allowed to be used; columns: the capacity of the knapsack
+  int rows = w.size() + 1;
+  int cols = W + 1;
+  vector<int> max_weights(rows * cols);
+
+  // Initialize the values in the 2D array
+  for (int item = 0; item < rows; ++item) {
+    max_weights[item * cols + 0] = 0;
+  }
+  for (int wt = 0; wt < cols; ++wt) {
+    max_weights[0 * cols + wt] = 0;
+  }
+
+  // DP Algo: filling the max_values row by row (is it okay to fill column by column?)
+  int wt_with_item = 0;
+  for (int item = 1; item < rows; ++item) {
+    for (int wt = 1; wt < cols; ++wt) {
+      max_weights[item * cols + wt] = max_weights[(item - 1) * cols + wt];
+      if (w[item - 1] <= wt) { // Note that we should use "i - 1" to access the ith item;
+        wt_with_item = max_weights[(item - 1) * cols + (wt - w[item - 1])] + w[item - 1];
+        if (max_weights[item * cols + wt] < wt_with_item) {
+          max_weights[item * cols + wt] = wt_with_item;
+        }
+      }
     }
   }
-  return current_weight;
+  // Reconstruct optimal solution (not yet implemented)
+
+  return max_weights[rows * cols - 1];
 }
 
 int main() {
