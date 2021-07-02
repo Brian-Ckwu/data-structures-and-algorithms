6cd077ca630dc37fc0d946cdaea53c0a46fefdb0
Brian Wu <sonny40920710@gmail.com>
2021-07-02 17:12:19 +0800

    Modify the start file of max_sliding_window problem

--- data_structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
+++ data_structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
@@ -6,31 +6,41 @@ using std::cout;
 using std::vector;
 using std::max;
 
-void max_sliding_window_naive(vector<int> const & A, int w) {
-    for (size_t i = 0; i < A.size() - w + 1; ++i) {
-        int window_max = A.at(i);
-        for (size_t j = i + 1; j < i + w; ++j)
-            window_max = max(window_max, A.at(j));
+// O(nm) naive solution
+vector<int> max_sliding_window_naive(const vector<int>& A, int w) {
+  vector<int> outputs(A.size() - w + 1);
+  
+  int window_max = 0;
+  for (size_t i = 0; i < A.size() - w + 1; ++i) {
+    window_max = A[i];
+    for (size_t j = i + 1; j < i + w; ++j)
+      window_max = max(window_max, A[j]);
+  
+    outputs[i] = window_max;
+  }
 
-        cout << window_max << " ";
-    }
-
-    return;
+  return outputs;
 }
+
+// O(n) solution
+vector<int> max_sliding_window_fast(const vector<int>& A, int w);
 
 
 int main() {
-    int n = 0;
-    cin >> n;
+  int n = 0;
+  cin >> n;
 
-    vector<int> A(n);
-    for (size_t i = 0; i < n; ++i)
-        cin >> A.at(i);
+  vector<int> A(n);
+  for (size_t i = 0; i < n; ++i)
+    cin >> A.at(i);
 
-    int w = 0;
-    cin >> w;
+  int w = 0;
+  cin >> w;
 
-    max_sliding_window_naive(A, w);
+  const vector<int> outputs = max_sliding_window_naive(A, w);
+  for (int output: outputs) {
+    cout << output << " ";
+  }
 
-    return 0;
+  return 0;
 }
