a8c5c8f34a40effeac31043044ca75db2c680249
Brian Wu <sonny40920710@gmail.com>
2021-07-02 16:01:32 +0800

    implement two problems using stacks

--- data_structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
+++ data_structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
@@ -3,43 +3,62 @@
 #include <string>
 
 struct Bracket {
-    Bracket(char type, int position):
-        type(type),
-        position(position)
-    {}
+  Bracket(char type, int position):
+    type(type),
+    position(position)
+  {}
 
-    bool Matchc(char c) {
-        if (type == '[' && c == ']')
-            return true;
-        if (type == '{' && c == '}')
-            return true;
-        if (type == '(' && c == ')')
-            return true;
-        return false;
-    }
+  bool Matchc(char c) {
+    if (type == '[' && c == ']')
+      return true;
+    if (type == '{' && c == '}')
+      return true;
+    if (type == '(' && c == ')')
+      return true;
+    return false;
+  }
 
-    char type;
-    int position;
+  char type;
+  int position;
 };
 
 int main() {
-    std::string text;
-    getline(std::cin, text);
+  std::string text;
+  getline(std::cin, text);
 
-    std::stack <Bracket> opening_brackets_stack;
-    for (int position = 0; position < text.length(); ++position) {
-        char next = text[position];
+  std::stack <Bracket> opening_brackets_stack;
+  std::string answer = "";
+  for (int position = 0; position < text.length(); ++position) {
+    char next = text[position];
 
-        if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
-        }
-
-        if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
-        }
+    if (next == '(' || next == '[' || next == '{') {
+      opening_brackets_stack.push(Bracket(next, position + 1));
     }
 
-    // Printing answer, write your code here
+    if (next == ')' || next == ']' || next == '}') {
+      if (opening_brackets_stack.empty() || !opening_brackets_stack.top().Matchc(next)) {
+        answer = std::to_string(position + 1);
+        break;
+      } else {
+        opening_brackets_stack.pop();
+      }
+    }
+  }
 
-    return 0;
+  // Check answer & stack
+  if (answer != "") {
+    // do nothing
+  } else if (opening_brackets_stack.empty()) {
+    answer = "Success";
+  } else {
+    while (!opening_brackets_stack.empty()) {
+      answer = std::to_string(opening_brackets_stack.top().position);
+      opening_brackets_stack.pop();
+    }
+  }
+
+  // Display answer
+  std::cout << answer;
+
+  return 0;
 }
--- data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.cpp
+++ data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.cpp
@@ -11,49 +11,53 @@ using std::cout;
 using std::max_element;
 
 class StackWithMax {
-    vector<int> stack;
+  vector<int> stack;
+  vector<int> stack_max; // auxiliary stack
 
   public:
 
-    void Push(int value) {
-        stack.push_back(value);
+  void Push(int value) {
+    stack.push_back(value);
+    if (stack_max.size() == 0 || value >= stack_max.back()) {
+      stack_max.push_back(value);
     }
+  }
 
-    void Pop() {
-        assert(stack.size());
-        stack.pop_back();
+  void Pop() {
+    assert(stack.size());
+    if (stack_max.back() == stack.back()) {
+      stack_max.pop_back();
     }
+    stack.pop_back();
+  }
 
-    int Max() const {
-        assert(stack.size());
-        return *max_element(stack.begin(), stack.end());
-    }
+  int Max() const { return stack_max.back(); } // O(1) time access to the maximum element
 };
 
 int main() {
-    int num_queries = 0;
-    cin >> num_queries;
+  int num_queries = 0;
+  cin >> num_queries;
 
-    string query;
-    string value;
+  string query;
+  string value;
 
-    StackWithMax stack;
+  StackWithMax stack;
 
-    for (int i = 0; i < num_queries; ++i) {
-        cin >> query;
-        if (query == "push") {
-            cin >> value;
-            stack.Push(std::stoi(value));
-        }
-        else if (query == "pop") {
-            stack.Pop();
-        }
-        else if (query == "max") {
-            cout << stack.Max() << "\n";
-        }
-        else {
-            assert(0);
-        }
+  for (int i = 0; i < num_queries; ++i) {
+    cin >> query;
+    if (query == "push") {
+      cin >> value;
+      stack.Push(std::stoi(value));
     }
-    return 0;
+    else if (query == "pop") {
+      stack.Pop();
+    }
+    else if (query == "max") {
+      cout << stack.Max() << "\n";
+    }
+    else {
+      assert(0);
+    }
+  }
+  return 0;
 }
