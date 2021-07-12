2a8cd08784f41281edfbdc796e920de9cf6493b6
Brian Wu <sonny40920710@gmail.com>
2021-07-12 15:50:21 +0800

    start implementing queue

--- data_structures/data_structures_implementation/queue/array_queue.cpp
+++ data_structures/data_structures_implementation/queue/array_queue.cpp
@@ -11,7 +11,61 @@ using namespace std;
 
 class MyQueue {
  public:
+  // functions: push(), pop(), front(), back(), empty(), and size()
+  MyQueue(size_t max_size);
+  ~MyQueue() { delete[] arr; }
+
+  int add_index(int n) { return (n < max_sz - 1) ? (n + 1) : 0; }
+  void push(int n);
+  void pop();
+  int& front() { return arr[front]; }
+  int& back() { return arr[back]; }
+  bool empty() { return front == back; }
+  size_t size() const { return (front <= back) ? (back - front) : (max_sz - front + back) ;}
+  size_t max_size() const { return max_sz; }
 
  private:
+  int front; // front index
+  int back;  // back index
+  int* arr;
+  size_t max_sz;
   
 };
+
+MyQueue::MyQueue(size_t max_size)
+  : front{0},
+    back{0},
+    max_sz{max_size} {
+      arr = new int[max_size];
+    }
+
+void MyQueue::push(int n) {
+  if (size() < max_sz - 1) {
+    arr[back] = n;
+    back = add_index(back);
+    return;
+  }
+
+  cout << "The queue is already full." << endl;
+  throw "ERROR";
+}
+
+void MyQueue::pop() {
+  if (!empty()) {
+    front = add_index(front);
+    return;
+  }
+
+  cout << "The queue is empty." << endl;
+  throw "ERROR";
+}
+
+void test_my_queue() {
+
+}
+
+int main() {
+  test_my_queue();
+
+  return 0;
+}
---
+++ data_structures/data_structures_implementation/queue/list_queue.cpp
@@ -0,0 +1,21 @@
+/*
+  Implementation of a queue using a linked-list
+
+  Advantage: no pre-allocated maximum size limitation
+  Limitation: additional space for storing pointers
+*/
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void test_my_queue() {
+
+}
+
+int main() {
+  test_my_queue();
+
+  return 0;
+}
