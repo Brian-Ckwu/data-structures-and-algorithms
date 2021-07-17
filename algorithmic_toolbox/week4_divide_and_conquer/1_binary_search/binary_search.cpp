4d1a5c37b31174d9671922676dee297cd2faef16
Brian Wu <sonny40920710@gmail.com>
2021-07-17 22:09:15 +0800

    implement binary search recursively

--- algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -4,12 +4,27 @@
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+int search(const vector<int>& a, int low, int high, int x) {
+  if (low > high) {
+    return -1;
+  }
+
+  int mid = low + (high - low) / 2;
+  if (a[mid] == x) {
+    return mid;
+  } else if (a[mid] > x) {
+    return search(a, low, mid - 1, x);
+  } else {
+    return search(a, mid + 1, high, x);
+  }
 }
 
-int linear_search(const vector<int> &a, int x) {
+int binary_search_recur(const vector<int>& a, int x) {
+  int low = 0, high = (int) a.size() - 1;
+  return search(a, low, high, x);
+}
+
+int linear_search(const vector<int>& a, int x) {
   for (size_t i = 0; i < a.size(); ++i) {
     if (a[i] == x) return i;
   }
@@ -31,6 +46,6 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search_recur(a, b[i]) << ' ';
   }
 }
