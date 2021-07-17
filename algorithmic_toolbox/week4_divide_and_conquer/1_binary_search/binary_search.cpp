86eb56cd2fc15fa0d9bc7b626a780f53706d5fe3
Brian Wu <sonny40920710@gmail.com>
2021-07-17 22:59:32 +0800

    implement binary search iteratively

--- algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -24,6 +24,24 @@ int binary_search_recur(const vector<int>& a, int x) {
   return search(a, low, high, x);
 }
 
+int binary_search_iter(const vector<int>& a, int x) {
+  int low = 0;
+  int high = (int) a.size() - 1;
+
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+    if (a[mid] == x) {
+      return mid;
+    } else if (a[mid] > x) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
 int linear_search(const vector<int>& a, int x) {
   for (size_t i = 0; i < a.size(); ++i) {
     if (a[i] == x) return i;
@@ -46,6 +64,6 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << binary_search_recur(a, b[i]) << ' ';
+    std::cout << binary_search_iter(a, b[i]) << ' ';
   }
 }
