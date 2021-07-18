f22332b248dca1e53f85d0b4edc29b126e7d6645
Brian Wu <sonny40920710@gmail.com>
2021-07-18 10:09:11 +0800

    implement heap_sort in test.cpp

--- data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/test.cpp
+++ data_structures/week2_priority_queues_and_disjoint_sets/1_make_heap/test.cpp
@@ -1,9 +1,25 @@
+/*
+  test std::make_heap
+*/
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-  size_t sz = 5;
+  vector<int> v = {4, 1, 7, 18, 3, 6, 0, 11, 14, 5};
+  make_heap(v.begin(), v.end());
 
-  cout << sz / 2;
+  // sort heap
+  for (int i = 0; i < v.size(); ++i) {
+    pop_heap(v.begin(), v.end() - i);
+  }
+
+  for (int& i : v) {
+    cout << i << " ";
+  }
+
+  return 0;
 }
