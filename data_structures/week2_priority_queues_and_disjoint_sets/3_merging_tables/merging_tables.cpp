5ccbbc24f597257faddba662a84287648b2fc12d
Brian Wu <sonny40920710@gmail.com>
2021-07-20 18:28:40 +0800

    implement disjoint set to solve merging_table problem

--- data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
+++ data_structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.cpp
@@ -20,7 +20,7 @@ struct DisjointSetsElement {
 struct DisjointSets {
 	int size;
 	int max_table_size;
-	vector <DisjointSetsElement> sets;
+	vector<DisjointSetsElement> sets;
 
 	DisjointSets(int size): size(size), max_table_size(0), sets(size) {
 		for (int i = 0; i < size; i++)
@@ -28,16 +28,33 @@ struct DisjointSets {
 	}
 
 	int getParent(int table) {
-		// find parent and compress path
+		if (table != sets[table].parent) {
+			sets[table].parent = getParent(sets[table].parent);
+		}
+		return sets[table].parent;
 	}
 
 	void merge(int destination, int source) {
 		int realDestination = getParent(destination);
 		int realSource = getParent(source);
 		if (realDestination != realSource) {
-			// merge two components
-			// use union by rank heuristic
-                        // update max_table_size
+			DisjointSetsElement& dest = sets[realDestination];
+			DisjointSetsElement& source = sets[realSource];
+			// use rank heuristic to merge to maintain efficiency of getParent (not necessary to attach real source to real destination)
+			if (dest.rank > source.rank) {
+				source.parent = dest.parent;
+				dest.size += source.size;
+			} else {
+				dest.parent = source.parent;
+				source.size += dest.size;
+				if (dest.rank == source.rank) {
+					++source.rank;
+				}
+			}
+			// without rank heuristic
+			// source.parent = dest.parent;
+			// dest.size += source.size;
+			max_table_size = max(max_table_size, max(dest.size, source.size));
 		}		
 	}
 };
@@ -47,7 +64,7 @@ int main() {
 	cin >> n >> m;
 
 	DisjointSets tables(n);
-	for (auto &table : tables.sets) {
+	for (auto& table : tables.sets) {
 		cin >> table.size;
 		tables.max_table_size = max(tables.max_table_size, table.size);
 	}
@@ -55,11 +72,12 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int destination, source;
 		cin >> destination >> source;
-                --destination;
-                --source;
-		
+		// change to 0-based index
+    --destination;
+    --source;
+		// merge tables
 		tables.merge(destination, source);
-	        cout << tables.max_table_size << endl;
+	  cout << tables.max_table_size << endl;
 	}
 
 	return 0;
