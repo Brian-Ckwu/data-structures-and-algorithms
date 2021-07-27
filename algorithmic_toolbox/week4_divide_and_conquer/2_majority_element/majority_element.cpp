f170cd3df5e8c1e543848cd427996ebd04eb9663
Brian Wu <sonny40920710@gmail.com>
2021-07-27 20:05:27 +0800

    implement finding the majority element using a hash map (time: O(n); space: O(n))

--- algorithmic_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ algorithmic_toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -1,13 +1,31 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
-using std::vector;
+using namespace std;
 
-int get_majority_element(vector<int> &a, int left, int right) {
+int get_majority_element(vector<int>& a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  return -1;
+}
+
+// Time complexity: O(n); Space complexity: O(n)
+int get_majority_element_hash(vector<int>& a) {
+  int half_size = a.size() / 2;
+  unordered_map<int, int> elem_count;
+  for (int i = 0; i < a.size(); ++i) {
+    if (elem_count.count(a[i])) {
+      ++elem_count[a[i]];
+    } else {
+      elem_count[a[i]] = 1;
+    }
+    if (elem_count[a[i]] > half_size) {
+      return a[i];
+    }
+  }
   return -1;
 }
 
@@ -18,5 +36,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element_hash(a) != -1) << '\n';
 }
