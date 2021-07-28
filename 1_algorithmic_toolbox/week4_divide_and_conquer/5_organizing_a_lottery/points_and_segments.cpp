1cebd3f9207ebf8014f9607555ebbce0d3705cdc
Brian Wu <sonny40920710@gmail.com>
2021-07-29 00:16:01 +0800

    solve points_and_segments by sorting and binary search

--- 1_algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ 1_algorithmic_toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
+int search_start(vector<int>& a, int point) {
+  int low = 0, high = a.size() - 1;
+  int mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (point >= a[mid]) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return ((low > mid) ? mid + 1 : mid);
+}
+
+int search_end(vector<int>& a, int point) {
+  int low = 0, high = a.size() - 1;
+  int mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (point <= a[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return ((high < mid) ? mid : mid + 1);
+}
+
+// O(nlogn) + O(nlogn) + 2n*O(logn) = O(nlogn)
 vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
   vector<int> cnt(points.size());
-  //write your code here
+  sort(starts.begin(), starts.end());
+  sort(ends.begin(), ends.end());
+  for (int i = 0; i < points.size(); ++i) { // search the position of "point" in sorted starts and ends
+    int start_pos = search_start(starts, points[i]);
+    int end_pos = search_end(ends, points[i]);
+    cnt[i] = start_pos - end_pos;
+  }
   return cnt;
 }
 
@@ -31,7 +67,7 @@ int main() {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
