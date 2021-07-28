e5801eacb4447ae4d1c333ed4dd621f8ea5b83e5
Brian Wu <sonny40920710@gmail.com>
2021-07-28 12:47:33 +0800

    implement quicksort by three-way-partition

--- algorithmic_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ algorithmic_toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+#include <bits/stdc++.h>
 
-using std::vector;
-using std::swap;
+using namespace std;
 
-int partition2(vector<int> &a, int l, int r) {
+int partition2(vector<int>& a, int l, int r) {
   int x = a[l];
   int j = l;
   for (int i = l + 1; i <= r; i++) {
@@ -18,17 +15,52 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
+pair<int, int> partition3(vector<int>& a, int l, int r) {
+  int p = a[l]; // pivot element
+  int m1 = l;
+  int m2 = l;
+  for (int i = l + 1; i <= r; ++i) {
+    if (a[i] < p) {
+      ++m1;
+      ++m2;
+      if (i > m2) {
+        swap(a[m1], a[m2]);
+      }
+      swap(a[m1], a[i]);
+    } else if (a[i] == p) {
+      ++m2;
+      swap(a[m2], a[i]);
+    } else {
+      // do nothing
+    }
+  }
+  swap(a[l], a[m1]);
+  return make_pair(m1, m2);
+}
+
+void randomized_quick_sort_2(vector<int>& a, int l, int r) {
   if (l >= r) {
     return;
   }
 
-  int k = l + rand() % (r - l + 1);
+  int k = l + rand() % (r - l + 1); // random pivot
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  int m = partition2(a, l, r); // partition2 or partition3
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort_2(a, l, m - 1);
+  randomized_quick_sort_2(a, m + 1, r);
+}
+
+void randomized_quick_sort_3(vector<int>& a, int l, int r) {
+  if (l >= r)
+    return;
+  // choose a random pivot
+  int k = l + rand() % (r - l + 1); // (r - l + 1) == number of elements
+  swap(a[l], a[k]);
+  pair<int, int> m = partition3(a, l, r);
+  // recursive call to quicksort
+  randomized_quick_sort_3(a, l, m.first - 1);
+  randomized_quick_sort_3(a, m.second + 1, r);
 }
 
 int main() {
@@ -38,7 +70,7 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  randomized_quick_sort_3(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
