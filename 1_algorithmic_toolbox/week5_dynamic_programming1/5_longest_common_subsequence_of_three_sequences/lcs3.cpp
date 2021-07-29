834489d40c2cc32d370b9b174bcba124a05e5c23
Brian Wu <sonny40920710@gmail.com>
2021-07-29 23:15:50 +0800

    solve longest common subsequence of 3 sequences using 3D DP

--- 1_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ 1_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -1,11 +1,33 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
-int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+int lcs3(vector<int>& a, vector<int>& b, vector<int>& c) {
+  // 3D DP array
+  int x = a.size();
+  int y = b.size();
+  int z = c.size();
+  int dp[x + 1][y + 1][z + 1];
+  // initialize the array
+  memset(dp, 0, sizeof(dp));
+  // bottom-up filling
+  for (int i = 1; i <= x; ++i) {
+    for (int j = 1; j <= y; ++j) {
+      for (int k = 1; k <= z; ++k) {
+        // ins == insertion
+        int insx = dp[i - 1][j][k];
+        int insy = dp[i][j - 1][k];
+        int insz = dp[i][j][k - 1];
+        int match = dp[i - 1][j - 1][k - 1] + 1;
+        if ((a[i - 1] == b[j - 1]) && (b[j - 1] == c[k - 1])) {
+          dp[i][j][k] = match;
+        } else {
+          dp[i][j][k] = max(max(insx, insy), insz);
+        }
+      }
+    }
+  }
+  return dp[x][y][z];
 }
 
 int main() {
