97d9472bd7b7ad88cba98a6b2eb72132bf378925
Brian Wu <sonny40920710@gmail.com>
2021-07-30 18:04:35 +0800

    implement bottom-up (tabulation) and top-down (memoization) DP algorithms for solving 0/1 knapsack

--- 1_algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ 1_algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// Could the algorithm be different 
+// initial implementation
 int optimal_weight(int W, const vector<int>& w) {
   // Vector for storing maximum values of subproblems
   // Rows: number of items allowed to be used; columns: the capacity of the knapsack
@@ -37,6 +36,48 @@ int optimal_weight(int W, const vector<int>& w) {
   return max_weights[rows * cols - 1];
 }
 
+// another iterative (bottom-up / tabulation) DP
+int optimal_weight_iter(int W, const vector<int>& w) {
+  // DP table
+  int n = w.size();
+  vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0)); // initialize to zeros
+  // fill the DP table
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= W; ++j) {
+      dp[i][j] = dp[i - 1][j];
+      if (j - w[i - 1] >= 0) {
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i - 1]] + w[i - 1]);
+      }
+    }
+  }
+  return dp[n][W];
+}
+
+// recursive (top-down / memoization) DP
+int optimal(int i, int W, const vector<int>& w, vector<vector<int>>& dp) {
+  if (dp[i][W] != -1)
+    return dp[i][W];
+  int max_weight = optimal(i - 1, W, w, dp);
+  if (w[i - 1] <= W)
+    max_weight = max(max_weight, optimal(i - 1, W - w[i - 1], w, dp) + w[i - 1]);
+  return (dp[i][W] = max_weight);
+}
+
+int optimal_weight_recur(int W, const vector<int>& w) {
+  // for memoization
+  int n = w.size();
+  vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+  // initialize the first row and the first column to zeros
+  dp[0][0] = 0;
+  for (int i = 1; i <= n; ++i) {
+    dp[i][0] = 0;
+  }
+  for (int j = 1; j <= W; ++j) {
+    dp[0][j] = 0;
+  }
+  return optimal(n, W, w, dp);
+} 
+
 int main() {
   int n, W;
   std::cin >> W >> n;
@@ -44,5 +85,5 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << optimal_weight_recur(W, w) << '\n';
 }
