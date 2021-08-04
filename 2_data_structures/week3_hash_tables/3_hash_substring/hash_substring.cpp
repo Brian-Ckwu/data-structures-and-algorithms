46a1757cccc61231517e169aad4b220051a0834c
Brian Wu <sonny40920710@gmail.com>
2021-08-04 10:19:02 +0800

    finish implementing Rabin-Karp algorithm

--- 2_data_structures/week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ 2_data_structures/week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-typedef unsigned long long ull;
+typedef long long ll;
 
 struct Data {
   string pattern, text;
@@ -19,25 +19,26 @@ void print_occurrences(const vector<int>& output) {
   std::cout << "\n";
 }
 
-ull polyhash(const string& text, int start, int end, ull prime, ull x) {
-  ull hash = 0;
+ll polyhash(const string& text, int start, int end, ll prime, ll x) {
+  ll hash = 0;
   for (int i = end; i >= start; --i) {
-    hash = ((int) text[i] + hash * x) % prime;
+    hash = ((ll) text[i] + hash * x) % prime;
   }
+  // cout << hash << endl;
   return hash;
 }
 
-vector<int> precompute_hashes(const string& text, int pattern_len, ull prime, ull x) {
+vector<ll> precompute_hashes(const string& text, int pattern_len, ll prime, ll x) {
   size_t n = text.size() - pattern_len;
-  vector<int> hashes(n + 1);
+  vector<ll> hashes(n + 1);
   hashes[n] = polyhash(text, n, n + pattern_len - 1, prime, x);
   // compute x ^ pattern_len
-  ull y = 1;
+  ll y = 1;
   for (int i = 0; i < pattern_len; ++i) {
     y = (y * x) % prime;
   }
   for (int i = n - 1; i >= 0; --i) {
-    hashes[i] = ((int) text[i] + x * hashes[i + 1] + y * (int) text[i + pattern_len]) % prime;
+    hashes[i] = ((((ll) text[i] + x * hashes[i + 1] - y * (ll) text[i + pattern_len]) % prime) + prime) % prime;
   }
   return hashes;
 }
@@ -53,11 +54,11 @@ bool are_equal(const string& pattern, const string& text, int start) {
 vector<int> get_occurrences(const Data& input) {
   const string& s = input.pattern, t = input.text;
   vector<int> ans;
-  ull prime = 956696761358687;
+  ll prime = 340029919604281;
   int x = rand() % 100 + 1;
   // compute p_hash & precompute t_hashes
-  ull p_hash = polyhash(s, 0, s.size() - 1, prime, x);
-  vector<int> t_hashes = precompute_hashes(t, s.size(), prime, x);
+  ll p_hash = polyhash(s, 0, s.size() - 1, prime, x);
+  vector<ll> t_hashes = precompute_hashes(t, s.size(), prime, x);
   // search pattern
   for (int i = 0; i <= t.size() - s.size(); ++i) {
     if (p_hash != t_hashes[i])
