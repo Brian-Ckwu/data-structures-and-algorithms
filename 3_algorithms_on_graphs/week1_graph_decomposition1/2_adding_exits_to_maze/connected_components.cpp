95dfb3bd6007f6b3b639d3c3aeb9db9b4a813250
Brian Wu <sonny40920710@gmail.com>
2021-09-21 01:50:39 +0800

    implement connected components algorithms

--- 3_algorithms_on_graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
+++ 3_algorithms_on_graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using std::vector;
 using std::pair;
+using std::unordered_set;
 
-int number_of_components(vector<vector<int> > &adj) {
+void explore(vector<vector<int>>& adj, unordered_set<int>& visited, int v) {
+  visited.insert(v);
+  for (int w : adj[v]) {
+    if (visited.count(w) == 0) {
+      explore(adj, visited, w);
+    }
+  }
+}
+
+int number_of_components(vector<vector<int>>& adj, unordered_set<int>& visited) {
   int res = 0;
-  //write your code here
+  for (int v = 0; v < adj.size(); ++v) {
+    if (visited.count(v) == 0) {
+      explore(adj, visited, v);
+      ++res;
+    }
+  }
   return res;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int>> adj(n, vector<int>()); // adjacency list
+  unordered_set<int> visited; // for storing visited vertices
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
     adj[y - 1].push_back(x - 1);
   }
-  std::cout << number_of_components(adj);
+  std::cout << number_of_components(adj, visited);
 }
