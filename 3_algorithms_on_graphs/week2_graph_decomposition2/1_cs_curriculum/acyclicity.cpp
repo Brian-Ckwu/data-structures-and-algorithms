c0d38e565bc0f6d0758c49b7e41ada2538266156
Brian Wu <sonny40920710@gmail.com>
2021-09-22 23:56:30 +0800

    initial attempt to solve acyclicity.cpp

--- 3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ 3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using std::vector;
 using std::pair;
+using std::unordered_set;
 
-int acyclic(vector<vector<int> > &adj) {
-  //write your code here
+int explore(vector<vector<int>>& adj, unordered_set<int>& visited, int v) {
+  visited.insert(v);
+  int cyclic = 0;
+  for (int w : adj[v]) {
+    if (visited.count(w) == 0) {
+      cyclic = explore(adj, visited, w);
+    } else {
+      return 1;
+    }
+  }
+  return cyclic;
+}
+
+int acyclic(vector<vector<int>>& adj, unordered_set<int>& visited) {
+  for (int v = 0; v < adj.size(); ++v) {
+    if (visited.count(v) == 0) {
+      if (explore(adj, visited, v) == 1) {
+        return 1;
+      }
+    }
+  }
   return 0;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int>> adj(n, vector<int>());
+  unordered_set<int> visited;
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
-  std::cout << acyclic(adj);
+  std::cout << acyclic(adj, visited);
 }
