d90132087decbe682f366a5f5881132265932de3
Brian Wu <sonny40920710@gmail.com>
2021-09-23 15:52:44 +0800

    counting strongly_connected_components

--- 3_algorithms_on_graphs/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
+++ 3_algorithms_on_graphs/week2_graph_decomposition2/3_intersection_reachability/strongly_connected.cpp
@@ -2,19 +2,54 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
-using std::pair;
+using namespace std;
 
-int number_of_strongly_connected_components(vector<vector<int> > adj) {
+void dfs(vector<vector<int>>& adj, vector<int>& visited, vector<int>& postorder, int v) {
+  visited[v] = 1;
+  for (int w : adj[v]) {
+    if (visited[w] == 0) {
+      dfs(adj, visited, postorder, w);
+    }
+  }
+  postorder.push_back(v);
+}
+
+int number_of_strongly_connected_components(vector<vector<int>> adj) {
+  // reverse the graph
+  int n = adj.size();
+  vector<vector<int>> radj(n, vector<int>());
+  for (int u = 0; u < n; ++u) {
+    for (int v : adj[u]) {
+      radj[v].push_back(u);
+    }
+  }
+  // run DFS on the reversed graph and record the post-order
+  vector<int> visited(n, 0);
+  vector<int> postorder;
+  for (int v = 0; v < n; ++v) {
+    if (visited[v] == 0) {
+      dfs(radj, visited, postorder, v);
+    }
+  }
+  // run DFS from the largest post-order
+  fill(visited.begin(), visited.end(), 0);
+  reverse(postorder.begin(), postorder.end());
+  vector<int> dummy;
   int result = 0;
-  //write your code here
+  for (int v : postorder) {
+    if (visited[v] == 0) {
+      dfs(adj, visited, dummy, v);
+      ++result;
+    }
+  }
+    // an explore == an SCC
   return result;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int>> adj(n, vector<int>());
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
