c8aab7b1053455c016855ee254bb3b9aa450530b
Brian Wu <sonny40920710@gmail.com>
2021-09-23 14:44:47 +0800

    initial successful attempt of acyclicity

--- 3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
+++ 3_algorithms_on_graphs/week2_graph_decomposition2/1_cs_curriculum/acyclicity.cpp
@@ -6,25 +6,28 @@ using std::vector;
 using std::pair;
 using std::unordered_set;
 
-int explore(vector<vector<int>>& adj, unordered_set<int>& visited, int v) {
+int check_cyclic(vector<vector<int>>& adj, unordered_set<int>& visited, unordered_set<int>& removed, int v) {
   visited.insert(v);
   int cyclic = 0;
   for (int w : adj[v]) {
-    if (visited.count(w) == 0) {
-      cyclic = explore(adj, visited, w);
-    } else {
-      return 1;
+    if (removed.count(w) == 0) {
+      if (visited.count(w) == 1) {
+        cyclic = 1;
+      } else {
+        cyclic = check_cyclic(adj, visited, removed, w);
+      }
     }
+    if (cyclic)
+      break;
   }
+  removed.insert(v);
   return cyclic;
 }
 
-int acyclic(vector<vector<int>>& adj, unordered_set<int>& visited) {
+int acyclic(vector<vector<int>>& adj, unordered_set<int>& visited, unordered_set<int>& removed) {
   for (int v = 0; v < adj.size(); ++v) {
-    if (visited.count(v) == 0) {
-      if (explore(adj, visited, v) == 1) {
-        return 1;
-      }
+    if (visited.count(v) == 0 && check_cyclic(adj, visited, removed, v) == 1) {
+      return 1;
     }
   }
   return 0;
@@ -35,10 +38,11 @@ int main() {
   std::cin >> n >> m;
   vector<vector<int>> adj(n, vector<int>());
   unordered_set<int> visited;
+  unordered_set<int> removed;
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
-  std::cout << acyclic(adj, visited);
+  std::cout << acyclic(adj, visited, removed);
 }
