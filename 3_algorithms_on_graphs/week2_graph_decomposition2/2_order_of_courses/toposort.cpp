52c0c73440876fc395b04436bf4f11720674ab07
Brian Wu <sonny40920710@gmail.com>
2021-09-23 15:00:53 +0800

    solve toposort.cpp

--- 3_algorithms_on_graphs/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
+++ 3_algorithms_on_graphs/week2_graph_decomposition2/2_order_of_courses/toposort.cpp
@@ -3,16 +3,27 @@
 #include <vector>
 
 using std::vector;
-using std::pair;
+using std::reverse;
 
-void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
-  //write your code here
+void dfs(vector<vector<int>>& adj, vector<int>& used, vector<int>& order, int x) {
+  for (int v : adj[x]) {
+    if (used[v] == 0) {
+      dfs(adj, used, order, v);
+    }
+  }
+  used[x] = 1;
+  order.push_back(x);
 }     
 
-vector<int> toposort(vector<vector<int> > adj) {
+vector<int> toposort(vector<vector<int>> adj) {
   vector<int> used(adj.size(), 0);
   vector<int> order;
-  //write your code here
+  for (int x = 0; x < adj.size(); ++x) {
+    if (used[x] == 0) {
+      dfs(adj, used, order, x);
+    }
+  }
+  reverse(order.begin(), order.end());
   return order;
 }
 
