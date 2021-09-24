1b54b97e805be995dff803a891028b34363cb0ff
Brian Wu <sonny40920710@gmail.com>
2021-09-24 15:34:09 +0800

    fix bugs in bipartite.cpp

--- 3_algorithms_on_graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ 3_algorithms_on_graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -3,25 +3,30 @@
 using namespace std;
 
 int bipartite(vector<vector<int>>& adj) {
+  int n = adj.size();
   // containers
   queue<int> q;
-  vector<int> color(adj.size(), -1);
-  // choose a random starting point
-  q.push(0);
-  color[0] = 0;
-  // color the graph
-  while (!q.empty()) {
-    int u = q.front(); q.pop();
-    for (int v : adj[u]) {
-      if (color[v] == -1) {
-        q.push(v);
-        color[v] = (color[u] + 1) % 2;
-      } else if (color[v] == color[u]) {
-        return 0;
+  vector<int> color(n, -1);
+  // loop through each connected component
+  for (int s = 0; s < n; ++s) {
+    if (color[s] == -1) {
+      // choose a random starting point
+      q.push(s);
+      color[s] = s;
+      // color the graph
+      while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (int v : adj[u]) {
+          if (color[v] == -1) {
+            q.push(v);
+            color[v] = (color[u] + 1) % 2;
+          } else if (color[v] == color[u]) { // check contradiction
+            return 0;
+          }
+        }
       }
     }
   }
-    // check contradiction
   return 1;
 }
 
