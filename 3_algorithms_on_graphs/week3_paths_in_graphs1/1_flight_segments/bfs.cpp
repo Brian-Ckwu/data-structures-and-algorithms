8a6716a10e03e720de9cbb0026e97acb8bae3657
Brian Wu <sonny40920710@gmail.com>
2021-09-24 14:56:08 +0800

    solve bfs.cpp

--- 3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ 3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -1,19 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 
-using std::vector;
-using std::queue;
+using namespace std;
 
-int distance(vector<vector<int> > &adj, int s, int t) {
-  //write your code here
-  return -1;
+int distance(vector<vector<int>>& adj, int s, int t) {
+  // container (queue for storing nodes & vector for storing distance)
+  queue<int> q;
+  vector<int> d(adj.size(), -1); // initialize distances to -1
+  // initialize starting point
+  q.push(s);
+  d[s] = 0;
+  // fill the distances
+  while (!q.empty()) {
+    int u = q.front(); q.pop();
+    for (int v : adj[u]) {
+      if (d[v] == -1) { // if node v hasn't been found
+        q.push(v);
+        d[v] = d[u] + 1;
+      }
+    }
+  }
+  return d[t];
 }
 
 int main() {
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int>> adj(n, vector<int>());
   for (int i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
