a3f86ebd3956144087b688a08cdac7f7ab89949f
Brian Wu <sonny40920710@gmail.com>
2021-09-25 13:06:06 +0800

    implement Prim's algorithm

--- 3_algorithms_on_graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
+++ 3_algorithms_on_graphs/week5_spanning_trees/1_connecting_points/connecting_points.cpp
@@ -1,15 +1,35 @@
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
 
-using std::vector;
+using namespace std;
 
 double minimum_distance(vector<int> x, vector<int> y) {
-  double result = 0.;
-  //write your code here
-  return result;
+  // containers
+  int n = x.size();
+  vector<double> cost(n, 100000000);
+  cost[0] = 0;
+  priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
+  vector<bool> visited(n, false);
+  // make queue
+  for (int i = 0; i < n; ++i) {
+    pq.push(make_pair(cost[i], i));
+  }
+  // Prim's algorithm
+  while (!pq.empty()) {
+    int u = pq.top().second; pq.pop(); // add node u to MST
+    if (!visited[u]) {
+      visited[u] = true;
+      for (int v = 0; v < n; ++v) { // traverse each edge
+        if (!visited[v]) {
+          double dist = sqrt(pow(x[u] - x[v], 2) + pow(y[u] - y[v], 2));
+          if (cost[v] > dist) {
+            cost[v] = dist;
+            pq.push(make_pair(dist, v));
+          }
+        }
+      }
+    }
+  }
+  return accumulate(cost.begin(), cost.end(), 0.0);
 } 
 
 int main() {
