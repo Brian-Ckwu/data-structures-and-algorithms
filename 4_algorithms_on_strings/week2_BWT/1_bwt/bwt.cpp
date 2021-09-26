b597b6c2f4fdb4925e5fb993cf347e34c5cc1ec5
Brian Wu <sonny40920710@gmail.com>
2021-09-27 00:12:52 +0800

    implement BWT algorithm

--- 4_algorithms_on_strings/week2_BWT/1_bwt/bwt.cpp
+++ 4_algorithms_on_strings/week2_BWT/1_bwt/bwt.cpp
@@ -8,12 +8,24 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using std::sort;
 
 string BWT(const string& text) {
+  int n = text.size();
+  // make the text cycle
+  string text_cycle = text + string(text.begin(), text.end() - 1);
+  // make the matrix
+  vector<string> matrix;
+  for (int i = 0; i < n; ++i) {
+    matrix.push_back(string(text_cycle.begin() + i, text_cycle.begin() + i + n));
+  }
+  // sort the matrix
+  sort(matrix.begin(), matrix.end());
+  // append the characters in the last column of the matrix to result
   string result = "";
-
-  // write your code here
-
+  for (int i = 0; i < n; ++i) {
+    result.push_back(matrix[i][n - 1]);
+  }
   return result;
 }
 
