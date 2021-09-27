45f0828e0a7e27744ed40ac3c4d201b2364bbd12
Brian Wu <sonny40920710@gmail.com>
2021-09-28 00:56:11 +0800

    implement Knuth-Morris-Pratt algorithm

--- 4_algorithms_on_strings/week3_KMP_and_suffix_array/1_kmp/kmp.cpp
+++ 4_algorithms_on_strings/week3_KMP_and_suffix_array/1_kmp/kmp.cpp
@@ -10,9 +10,36 @@ using std::vector;
 // Find all occurrences of the pattern in the text and return a
 // vector with all positions in the text (starting from 0) where 
 // the pattern starts in the text.
+
+vector<int> compute_prefix(const string& pnt) {
+  int n = pnt.size();
+  vector<int> s(n, 0);
+  int border = 0;
+  for (int i = 1; i < n; ++i) {
+    while ((border > 0) && (pnt[i] != pnt[border])) {
+      border = s[border - 1];
+    }
+    if (pnt[i] == pnt[border]) {
+      ++border;
+    } else {
+      border = 0;
+    }
+    s[i] = border;
+  }
+  return s;
+}
+
+// Knuth-Morris-Pratt Algorithm: O(|pattern| + |text|)
 vector<int> find_pattern(const string& pattern, const string& text) {
+  string pnt = pattern + '$' + text;
+  vector<int> s = compute_prefix(pnt);
   vector<int> result;
-  // Implement this function yourself
+  int p = pattern.size(), n = pnt.size();
+  for (int i = p + 1; i < n; ++i) {
+    if (s[i] == p) {
+      result.push_back(i - 2 * p);
+    }
+  }
   return result;
 }
 
