87e6a5968d3d83bf41d6fe6c9cc2d41056effdd6
Brian Wu <sonny40920710@gmail.com>
2021-09-27 23:45:46 +0800

    week3_KMP_and_suffix_array

--- 4_algorithms_on_strings/week2_BWT/2_bwtinverse/bwtinverse.cpp
+++ 4_algorithms_on_strings/week2_BWT/2_bwtinverse/bwtinverse.cpp
@@ -1,18 +1,52 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+using namespace std;
 
 string InverseBWT(const string& bwt) {
+  int n = bwt.size();
+  string lcol(bwt.begin(), bwt.end());
+  string fcol(bwt.begin(), bwt.end());
+  sort(fcol.begin(), fcol.end());
+  // storing positions
+  unordered_map<char, vector<int>> lcol_pos;
+  vector<pair<char, int>> lcol_pair;
+  for (int i = 0; i < n; ++i) {
+    char ch = lcol[i];
+    if (lcol_pos.count(ch)) {
+      lcol_pos[ch].push_back(i);
+    } else {
+      lcol_pos[ch] = vector<int>(1, i);
+    }
+    lcol_pair.push_back(make_pair(ch, lcol_pos[ch].size() - 1));
+  }
+  unordered_map<char, vector<int>> fcol_pos; // this data structure is not required actually
+  vector<pair<char, int>> fcol_pair;
+  for (int i = 0; i < n; ++i) {
+    char ch = fcol[i];
+    if (fcol_pos.count(ch)) {
+      fcol_pos[ch].push_back(i);
+    } else {
+      fcol_pos[ch] = vector<int>(1, i);
+    }
+    fcol_pair.push_back(make_pair(ch, fcol_pos[ch].size() - 1));
+  }
+  // reconstructing the text
+  // finding the $ symbol
+  int symbol_pos;
+  for (int i = 0; i < n; ++i) {
+    if (lcol[i] == '$') {
+      symbol_pos = i;
+    }
+  }
+  // start reconstructing
   string text = "";
-
-  // write your code here
+  for (int j = 0, pos = symbol_pos; j < n; ++j) {
+    pair<char, int> ch_pair = fcol_pair[pos];
+    char ch = ch_pair.first;
+    int i = ch_pair.second;
+    text.push_back(ch);
+    pos = lcol_pos[ch][i];
+  }
 
   return text;
 }
