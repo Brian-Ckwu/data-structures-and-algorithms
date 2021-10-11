26d56c71d0e7873f614b071fc5342b9c02589b24
Brian Wu <sonny40920710@gmail.com>
2021-10-11 16:41:00 +0800

    solve weighted independence set in trees by dynamic programming

--- 5_advanced_algorithms_and_complexity/week4_coping_with_NP_completeness/2_plan_party/plan_party.cpp
+++ 5_advanced_algorithms_and_complexity/week4_coping_with_NP_completeness/2_plan_party/plan_party.cpp
@@ -1,74 +1,89 @@
-#include <iostream>
-#include <vector>
+#include <sys/resource.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 struct Vertex {
-    int weight;
-    std::vector <int> children;
+  int weight;
+  vector<int> children;
 };
-typedef std::vector<Vertex> Graph;
-typedef std::vector<int> Sum;
+typedef vector<Vertex> Graph;
+typedef vector<int> Sum;
 
 Graph ReadTree() {
-    int vertices_count;
-    std::cin >> vertices_count;
+  int vertices_count;
+  cin >> vertices_count;
 
-    Graph tree(vertices_count);
+  Graph tree(vertices_count);
 
-    for (int i = 0; i < vertices_count; ++i)
-        std::cin >> tree[i].weight;
+  for (int i = 0; i < vertices_count; ++i)
+    cin >> tree[i].weight;
 
-    for (int i = 1; i < vertices_count; ++i) {
-        int from, to, weight;
-        std::cin >> from >> to;
-        tree[from - 1].children.push_back(to - 1);
-        tree[to - 1].children.push_back(from - 1);
+  for (int i = 1; i < vertices_count; ++i) {
+    int from, to, weight;
+    cin >> from >> to;
+    tree[from - 1].children.push_back(to - 1);
+    tree[to - 1].children.push_back(from - 1);
+  }
+
+  return tree;
+}
+
+void dfs(const Graph& tree, int vertex, int parent, vector<int>& fun_sum) {
+  for (int child : tree[vertex].children) {
+    if (child != parent) {
+      dfs(tree, child, vertex, fun_sum);
     }
-
-    return tree;
+  }
+  // calculate the maximum sum of fun factors
+  int m1 = tree[vertex].weight; // maximum sum of the fun factors including the vertex
+  int m0 = 0; // maximum sum of the fun factors excluding the vertex
+  for (int child : tree[vertex].children) {
+    if (child != parent) {
+      m0 += fun_sum[child];
+      for (int grandchild : tree[child].children) {
+        if (grandchild != vertex) {
+          m1 += fun_sum[grandchild];
+        }
+      }
+    }
+  }
+  // record the maximum sum
+  fun_sum[vertex] = max(m0, m1);
 }
 
-void dfs(const Graph &tree, int vertex, int parent) {
-    for (int child : tree[vertex].children)
-        if (child != parent)
-            dfs(tree, child, vertex);
-
-    // This is a template function for processing a tree using depth-first search.
-    // Write your code here.
-    // You may need to add more parameters to this function for child processing.
-}
-
-int MaxWeightIndependentTreeSubset(const Graph &tree) {
-    size_t size = tree.size();
-    if (size == 0)
-        return 0;
-    dfs(tree, 0, -1);
-    // You must decide what to return.
+int MaxWeightIndependentTreeSubset(const Graph& tree) {
+  size_t size = tree.size();
+  if (size == 0)
     return 0;
+  vector<int> fun_sum(size, 0);
+  dfs(tree, 0, -1, fun_sum);
+  return fun_sum[0];
 }
 
 int main() {
-    // This code is here to increase the stack size to avoid stack overflow
-    // in depth-first search.
-    const rlim_t kStackSize = 64L * 1024L * 1024L;  // min stack size = 64 Mb
-    struct rlimit rl;
-    int result;
-    result = getrlimit(RLIMIT_STACK, &rl);
-    if (result == 0)
+  // This code is here to increase the stack size to avoid stack overflow
+  // in depth-first search.
+  const rlim_t kStackSize = 64L * 1024L * 1024L;  // min stack size = 64 Mb
+  struct rlimit rl;
+  int result;
+  result = getrlimit(RLIMIT_STACK, &rl);
+  if (result == 0)
+  {
+    if (rl.rlim_cur < kStackSize)
     {
-        if (rl.rlim_cur < kStackSize)
-        {
-            rl.rlim_cur = kStackSize;
-            result = setrlimit(RLIMIT_STACK, &rl);
-            if (result != 0)
-            {
-                fprintf(stderr, "setrlimit returned result = %d\n", result);
-            }
-        }
+      rl.rlim_cur = kStackSize;
+      result = setrlimit(RLIMIT_STACK, &rl);
+      if (result != 0)
+      {
+        fprintf(stderr, "setrlimit returned result = %d\n", result);
+      }
     }
+  }
 
-    // Here begins the solution
-    Graph tree = ReadTree();
-    int weight = MaxWeightIndependentTreeSubset(tree);
-    std::cout << weight << std::endl;
-    return 0;
+  // Here begins the solution
+  Graph tree = ReadTree();
+  int weight = MaxWeightIndependentTreeSubset(tree);
+  cout << weight << endl;
+  return 0;
 }
